80b5fabb962a3ee90e1660a4bddc3bff79a64534
jobenhaim <145897383+jobenhaim@users.noreply.github.com>
2026-07-05 11:53:36 +0300

    feat: configure splash screen auto-hide
    Disable launch delay for a seamless app startup experience.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.oku.sudoku',
   appName: 'Oku',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0,
+      launchAutoHide: true
+    }
+  }
 };
 
 export default config;
